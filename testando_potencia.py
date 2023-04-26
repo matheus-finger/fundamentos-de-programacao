e39685d71c2092645b2559268bded33007bdb89c
mafefreeitas <mfernandafg4@gmail.com>
2023-04-26 11:10:45 -0300

    Revert "Adicionando arquivos da aula do dia 26/04"
    This reverts commit 8a4e89a6f03ff0266fd6038213a24f35c7b3be74.

--- testando_potencia.py
+++
@@ -1,8 +0,0 @@
-# ele vai achar porque a função está num arquivo do mesmo diretório
-from potencia import pow
-
-print(pow(2, 3))
-# resultado é 8
-
-print(pow(2))
-# resultado é 4, pois não foi passado valor para b, então ele, usa o default definido na função (2)"
